f247d22ee94716b349c9013e6b70b61fde740e8d
OliverCadman <o.cadman@live.co.uk>
2021-05-20 17:06:34 +0100

    Placed all shop image files into one single folder. Added JavaScript to make Shop page interactive and relevant

--- shop.js
+++ shop.js
@@ -21,17 +21,58 @@ cartBtn.forEach(function(btn) {
         if(event.target.classList.contains('addbasket-button')) {
             let fullPath = 
             event.target.parentElement.previousElementSibling.firstElementChild.firstElementChild.src;
+
+            console.log(fullPath)
         
-            let pos = fullPath.indexOf('album-covers') + 12;
+            let pos = fullPath.indexOf('shop-images') + 11;
+
+            // console.log(pos)
+
+
             let partPath = fullPath.slice(pos);
 
+            console.log(partPath);
+
             const item = {};
-            item.img = `img-cart${partPath}`;
-            let name = event.target.parentElement.parentElement.previousElementSibling;
+            console.log(item)
+            item.img = `assets/images/img-cart${partPath}`;
+            let name = event.target.parentElement.parentElement.children[0].children[0].textContent;
+            item.name = name;
+            let price = event.target.parentElement.parentElement.children[1].children[1].textContent;
+            item.price = price;
+            let finalPrice = price.slice(1).trim();
 
-            console.log(name);
+            item.price = finalPrice;
 
-            console.log(item);
+            // console.log(item);
+
+            const cartItem = document.createElement('div');
+            cartItem.classList.add('cart-item', 
+                                'd-flex', 
+                                'justify-content-start', 
+                                'my-3');
+
+            cartItem.innerHTML = `
+                <img src="${item.img}" id="item-img" alt="">
+                <div class="cart-item d-flex justify-content-between text-capitalize my-3">
+                    <div class="item-text">
+                        <p id="cart-item-title" class="font-weight-bold ml-3 mb-0">${item.name}</p>
+                        <span class="ml-3">£</span>
+                        <span id="cart-item-price" class="cart-item-price mb-0">${item.price}</span>
+                    </div>
+                    <a href="#" id="cart-item-remove" class="cart-item-remove">
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>`;
+
+            // Select Cart
+
+            const cart = document.getElementById('cart');
+            const total = document.querySelector('.cart-total-container');
+
+            cart.insertBefore(cartItem, total);
+            showTotals();
             
 
 
@@ -39,6 +80,29 @@ cartBtn.forEach(function(btn) {
     });
 });
 
+    // Show Total Price
+    function showTotals(){
+       
+        const total =[];
+        const items = document.querySelectorAll('.cart-item-price');
 
+        items.forEach(function(item) {
+            total.push(parseFloat(item.textContent));
+        })
+
+        const totalMoney = total.reduce(function(total,item){
+            total += item;
+            return total;
+        },0);
+
+        const finalMoney = totalMoney.toFixed(2);
+
+
+       
+
+        document.getElementById('cart-total').textContent = finalMoney;
+        document.querySelector('.item-total').textContent = finalMoney;
+        document.getElementById('item-count').textContent = total.length;
+    }
 
 })();
