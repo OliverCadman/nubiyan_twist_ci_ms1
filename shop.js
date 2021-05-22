7fc0be4a141100fa7f4e5fed5472956f956c61f9
OliverCadman <o.cadman@live.co.uk>
2021-05-22 13:24:44 +0100

    Styled basket info button on mobile screen size. Made basket expand to the full width of the screen, and added a close button to basket.

--- shop.js
+++ shop.js
@@ -1,107 +1,111 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-} else {
-    ready()
-}
-
-function ready() {
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-            
-    }
-
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
-
-    var addToCartButtons = document.getElementsByClassName('addbasket-button')
-    for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
-    }
-
-}
-
-function removeCartItem(event) {
-    var buttonClicked = event.target
-            buttonClicked.parentElement.parentElement.remove();
-            updateCartTotal()
-}
-
-function quantityChanged(event) {
-    var input = event.target 
-    if (input.value <= 0) {
-        input.value = 1
-    }
-    updateCartTotal()
-}
-
-function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement.children[0].children[0].innerText;
-    var price = button.parentElement.parentElement.children[1].children[1].innerText;
-    var imageSrc = button.parentElement.previousElementSibling.firstElementChild.firstElementChild.src;
-    console.log(shopItem, price, imageSrc)
-  
-}
-
-function addItemToCart(shopItem, price, imageSrc) {
-    var cartRow = document.createElement('div');
-    cartRow.classList.add('cart-row');
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    cartRow.innerHTML = `
-    <div class="cart-item cart-column">
-        <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-        <span class="cart-item-title">${shopItem}</span>
-    </div>
-    <span class="cart-price cart-column">${price}</span>
-  
-        <button class="btn btn-danger" type="button">REMOVE</button>
-        `
-
-    cartItems.append(cartRow)
-}
-
-function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$',''));
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-    }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '£' + total
-}
-
-
-
-
-
-
-
-
-
 // Show Cart
 
 (function(){
-const cartInfo = document.getElementById('cart-info');
-const cart = document.getElementById('cart');
-
-
-cartInfo.addEventListener('click', function(){
-    cart.classList.toggle('show-cart');
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
+    const hideCart = document.getElementById('close-basket')
     
-})
-})();
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    })
+
+    hideCart.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    })
+    })();
+ // Add Items to Cart
+    
+    (function (){
+    const cartBtn = document.querySelectorAll('.addbasket-button');
+    
+    cartBtn.forEach(function(btn) {
+        btn.addEventListener('click', function(event) {
+            //console.log(event.target);
+    
+            if(event.target.classList.contains('addbasket-button')) {
+                let fullPath = 
+                event.target.parentElement.previousElementSibling.firstElementChild.firstElementChild.src;
+    
+                console.log(fullPath)
+            
+                let pos = fullPath.indexOf('shop-images') + 11;
+    
+                // console.log(pos)
+    
+    
+                let partPath = fullPath.slice(pos);
+    
+                console.log(partPath);
+    
+                const item = {};
+                console.log(item)
+                item.img = `assets/images/img-cart${partPath}`;
+                let name = event.target.parentElement.parentElement.children[0].children[0].textContent;
+                item.name = name;
+                let price = event.target.parentElement.parentElement.children[1].children[1].textContent;
+                item.price = price;
+                let finalPrice = price.slice(1).trim();
+    
+                item.price = finalPrice;
+    
+                // console.log(item);
+    
+                const cartItem = document.createElement('div');
+                cartItem.classList.add('cart-item', 
+                                    'd-flex', 
+                                    'justify-content-start', 
+                                    'my-3');
+    
+                cartItem.innerHTML = `
+                    <img src="${item.img}" id="item-img" alt="">
+                    <div class="cart-item d-flex justify-content-between text-capitalize my-md-3">
+                        <div class="item-text">
+                            <p id="cart-item-title" class="font-weight-bold ml-3 mb-0"><span class="block">${item.name}</span></p>
+                            <span class="ml-3">£</span>
+                            <span id="cart-item-price" class="cart-item-price mb-0">${item.price}</span>
+                        </div>
+                        <div>
+                        <button type="button" class="btn btn-danger">Remove</button>
+                        </div>
+                    </div>
+                </div>`;
+    
+                // Select Cart
+    
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
+    
+                cart.insertBefore(cartItem, total);
+                showTotals();
+                
+    
+    
+            }
+        });
+    });
+    
+        // Show Total Price
+        function showTotals(){
+           
+            const total =[];
+            const items = document.querySelectorAll('.cart-item-price');
+    
+            items.forEach(function(item) {
+                total.push(parseFloat(item.textContent));
+            })
+    
+            const totalMoney = total.reduce(function(total,item){
+                total += item;
+                return total;
+            },0);
+    
+            const finalMoney = totalMoney.toFixed(2);
+    
+            document.getElementById('cart-total').textContent = finalMoney;
+            document.getElementById('item-count').textContent = '(' + total.length + ')';
+        }
+    
+    })();
 
 
-
+    
